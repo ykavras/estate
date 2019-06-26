ce32c4f42174c8217601c911f716fb1a2ae56415
ykavras <ykavras@gmail.com>
2019-06-27 00:49:44 +0300

    edited gitignore

--- estate/apps/core/urls.py
+++ estate/apps/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import core
+from .views import core, add_comments
 
 app_name = 'core'
 
 urlpatterns = [
     path('', core, name='core'),
+    path('test', add_comments, name='add_comments'),
 ]
--- estate/apps/core/views.py
+++ estate/apps/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import json
 
 
 def core(request):
@@ -6,3 +7,18 @@ def core(request):
 
     }
     return render(request, 'core.html', payload)
+
+
+def add_comments(request):
+    if 'application/x-www-form-urlencoded' in request.META['CONTENT_TYPE']:
+        print('hi')
+        data = json.loads(request.body)
+        comment = data.get('comment', None)
+        id = data.get('id', None)
+        title = data.get('title', None)
+
+        post = 'test'
+        com = Comment()
+        com.comments = comment
+        com.title = post
+        com.save()
