6ad7dc612c210e71d6a07b83c7341c7fbcf955ed
ishakoktn@gmail.com <ishak.okutan@orgino.com.tr>
2019-07-01 02:27:54 +0300

    edit uploading image from drf

--- estate/api/screens.py
+++ estate/api/screens.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers, viewsets
 from rest_framework.permissions import IsAuthenticated
+from drf_extra_fields.fields import Base64ImageField
 
 from estate.apps.screen.models import Screen, HotSpot
 
 
 class ScreenSerializer(serializers.ModelSerializer):
+    image = Base64ImageField(max_length=None, use_url=True, required=False)
+
     class Meta:
         model = Screen
         fields = '__all__'
