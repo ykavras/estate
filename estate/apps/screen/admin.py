8ced2b1003138db29da895dd114dd7f96cb85bfd
ishakoktn@gmail.com <ishak.okutan@orgino.com.tr>
2019-09-09 19:31:52 +0300

    set screen detail page with rendering template

--- estate/apps/screen/admin.py
+++ estate/apps/screen/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import *
 
 
+class HotSpotAdmin(admin.ModelAdmin):
+    list_display = ('text', 'screen')
+    list_filter = ('screen', )
+
 admin.site.register(Screen)
-admin.site.register(HotSpot)
+admin.site.register(HotSpot, HotSpotAdmin)
--- estate/apps/screen/models.py
+++ estate/apps/screen/models.py
@@ -25,6 +25,9 @@ class Screen(models.Model):
     def get_absolute_url(self):
         return reverse('reverse:reverse', args=self.pk)
 
+    def scene_hotspots(self):
+        return self.hotspots.filter(type='scene')
+
     class Meta:
         verbose_name = 'Screen'
         verbose_name_plural = 'Screens'
@@ -33,20 +36,22 @@ class Screen(models.Model):
 
 class HotSpot(models.Model):
     type_choices = (('info', 'info'), ('scene', 'scene'))
-    screen = models.ForeignKey(Screen, on_delete=models.CASCADE, related_name='hotspots')
+    screen = models.ForeignKey(Screen, on_delete=models.CASCADE, related_name='hotspots',
+                               help_text='Hangi ekranda gösterilecek')
+    target_screen = models.ForeignKey(Screen, on_delete=models.CASCADE, related_name='my_links',
+                                      verbose_name='Hedef Ekran', help_text='Sadece tip "scene" ise doldurunuz',
+                                      null=True, blank=True)
     pitch = models.CharField(max_length=255, verbose_name="pitch")
     yaw = models.CharField(max_length=255, verbose_name="yaw")
     type = models.CharField(verbose_name='Types', max_length=5, choices=type_choices, null=True, blank=True, )
     text = models.CharField(max_length=255, null=True, blank=True, help_text='If type is info fill here')
-    screenId = models.CharField(verbose_name='Screen Id', max_length=255, null=True, blank=True,
-                                help_text='If type is screen fill here')
     url = models.URLField(null=True, blank=True, help_text='If type is info then you may give link')
 
     def __str__(self):
         return self.text
 
-    def get_absolute_url(self):
-        return ''
+    def costum_screen_id(self):
+        return self.target_screen.name if self.target_screen else self.screen.name
 
     class Meta:
         verbose_name = 'HotSpot'
